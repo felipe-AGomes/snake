50e530514598cb94761588a2e77929910444c052
Felipe <falmeidagomes13@gmail.com>
2023-07-20 16:16:49 -0300

    feat: project configuration

---
+++ src/app/layout.tsx
@@ -0,0 +1,15 @@
+import './globals.css';
+import Head from './head';
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang='pt-br'>
+			<Head />
+			<body>{children}</body>
+		</html>
+	);
+}
