e5332496aeef690f4b57b25911b1a73f6b8663b4
Felipe <falmeidagomes13@gmail.com>
2023-07-23 14:48:50 -0300

    feat: add IUtil interface, and remove static methods

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,14 @@
-export class Util {
-	static randomNumberBetween(min: number, max: number) {
+export interface IUtil {
+	randomNumberBetween(min: number, max: number): number;
+	randomNumberUpTo(value: number): number;
+}
+
+export class Util implements IUtil {
+	public randomNumberBetween(min: number, max: number): number {
 		return Math.round(Math.random() * (max - min)) + min;
 	}
 
-	static randomNumberUpTo(value: number) {
+	public randomNumberUpTo(value: number): number {
 		return Math.round(Math.random() * value);
 	}
 }
